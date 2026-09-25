42e9721f8a6d1eb7fcfd3441078718954231f319
PH536-UI <Paulohenrique2020pereira@gmail.com>
2026-09-25 17:25:49 +0000

    fix: corrige sintaxe terraform Networking - multiline blocks

--- 4-networking/dns/main.tf
+++ 4-networking/dns/main.tf
@@ -1,10 +1,17 @@
-terraform { required_providers { oci = { source = "oracle/oci" } } }
+terraform {
+  required_providers {
+    oci = { source = "oracle/oci" }
+  }
+}
 provider "oci" {
   tenancy_ocid = "ocid1.tenancy.oc1..flocilocaltenancy0000000000000000000000000000000000000000"
-  user_ocid = "ocid1.user.oc1..anyuser" fingerprint = "aa:bb:cc:dd:ee:ff:00:11:22:33:44:55:66:77:88:99"
-  private_key_path = "/tmp/fake.pem" region = "us-ashburn-1"
+  user_ocid = "ocid1.user.oc1..anyuser"
+  fingerprint = "aa:bb:cc:dd:ee:ff:00:11:22:33:44:55:66:77:88:99"
+  private_key_path = "/tmp/fake.pem"
+  region = "us-ashburn-1"
 }
 resource "oci_dns_zone" "public_zone" {
   compartment_id = "ocid1.tenancy.oc1..flocilocaltenancy0000000000000000000000000000000000000000"
-  name = "floci.local" zone_type = "PRIMARY"
+  name = "floci.local"
+  zone_type = "PRIMARY"
 }
--- 4-networking/service-gateway/main.tf
+++ 4-networking/service-gateway/main.tf
@@ -1,13 +1,21 @@
-terraform { required_providers { oci = { source = "oracle/oci" } } }
+terraform {
+  required_providers {
+    oci = { source = "oracle/oci" }
+  }
+}
 provider "oci" {
   tenancy_ocid = "ocid1.tenancy.oc1..flocilocaltenancy0000000000000000000000000000000000000000"
-  user_ocid = "ocid1.user.oc1..anyuser" fingerprint = "aa:bb:cc:dd:ee:ff:00:11:22:33:44:55:66:77:88:99"
-  private_key_path = "/tmp/fake.pem" region = "us-ashburn-1"
+  user_ocid = "ocid1.user.oc1..anyuser"
+  fingerprint = "aa:bb:cc:dd:ee:ff:00:11:22:33:44:55:66:77:88:99"
+  private_key_path = "/tmp/fake.pem"
+  region = "us-ashburn-1"
 }
 data "oci_core_services" "all" {}
 resource "oci_core_service_gateway" "sgw" {
   compartment_id = "ocid1.tenancy.oc1..flocilocaltenancy0000000000000000000000000000000000000000"
   vcn_id = "ocid1.vcn.oc1..flocilocal"
   display_name = "ServiceGateway-ObjectStorage"
-  services { service_id = data.oci_core_services.all.services[0].id }
+  services {
+    service_id = data.oci_core_services.all.services[0].id
+  }
 }
